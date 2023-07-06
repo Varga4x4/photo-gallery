2ca9adab23676f37fc04a3fad8ea0426bad52f9c
Gabor Varga <varga4x4@outlook.com>
2023-07-06 18:15:56 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const main = () => {
+
+
+
+
+
+
+
+
+
+    const imageGiulia105Element = document.getElementById("giulia-105")
+    console.log(imageGiulia105Element)
+
+    const image159Element = document.getElementById("159")
+
+    const imageGiulia952Element = document.getElementById("giulia-952")
+
+    const imageDuettoElement = document.getElementById("duetto")
+
+    const image916Element = document.getElementById("916")
+
+    const image939Element = document.getElementById("939")
+
+    const imageTipo33Element = document.getElementById("tipo-33")
+
+    const image8cElement = document.getElementById("8-c")
+
+    const image4cElement = document.getElementById("4-c")
+}
+
+main()
